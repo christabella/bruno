145244c626f6b3b2977c39f7fb4ba64d016b37e5
Irwanto Christabella <irwantc1@login2.triton.aalto.fi>
2019-12-04 22:35:58 +0200

    Update gpu_job.sh with 2 GPU's and lr, glow parameters.

--- gpu_job.sh
+++ gpu_job.sh
@@ -4,12 +4,11 @@
 # conda activate bruno_v2
 # srun --gres=gpu:1 --mem=4G python3 -m config_conditional.train  --config_name m1_shapenet_glow --nr_gpu 1
 
-
-#SBATCH --time=12:00:00          ## wallclock time hh:mm:ss
 #SBATCH --gres=gpu:2        ## one K80 requested
 #SBATCH --mem=4G            ## one K80 requested
 
 # srun python3 -m config_conditional.train  --config_name m1_shapenet_glow --nr_gpu 2
 # srun --time=0:10:00 --gres=gpu:1 --mem=2G guild run -y conditional_bruno:train config_name=m1_shapenet_glow
-srun --time=0:10:00 --gres=gpu:1 --mem=2G guild run -y conditional_bruno:train config_name=m1_shapenet
+srun --time=30:00:00 --gres=gpu:2 --mem=2G guild run -y conditional_bruno:train \
+     config_name=m1_shapenet nr_gpu=2 learning_rate=1e-5
 # srun --gres=gpu:1 guild run -y simple:train
