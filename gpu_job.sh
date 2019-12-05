f09c508956ec0e75b7a2d40416b0ab02c6adec92
Irwanto Christabella <irwantc1@login2.triton.aalto.fi>
2019-12-05 12:15:52 +0200

    Those runs I submitted were realNVP... (Are you kidding me).

--- gpu_job.sh
+++ gpu_job.sh
@@ -4,11 +4,12 @@
 # conda activate bruno_v2
 # srun --gres=gpu:1 --mem=4G python3 -m config_conditional.train  --config_name m1_shapenet_glow --nr_gpu 1
 
+#SBATCH --time=30:00:00
 #SBATCH --gres=gpu:2        ## one K80 requested
-#SBATCH --mem=4G            ## one K80 requested
+#SBATCH --mem=3G            ## one K80 requested
 
 # srun python3 -m config_conditional.train  --config_name m1_shapenet_glow --nr_gpu 2
 # srun --time=0:10:00 --gres=gpu:1 --mem=2G guild run -y conditional_bruno:train config_name=m1_shapenet_glow
 srun --time=30:00:00 --gres=gpu:2 --mem=2G guild run -y conditional_bruno:train \
-     config_name=m1_shapenet nr_gpu=2 learning_rate=1e-5
+     config_name=m1_shapenet_glow nr_gpu=2 learning_rate=1e-5
 # srun --gres=gpu:1 guild run -y simple:train
